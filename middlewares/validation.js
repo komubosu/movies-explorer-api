a6dd3585224f2208b599a60d0f72a5dbdc1418cc
Michael Dokuchaev <redisooon@ya.ru>
2021-10-26 16:20:24 +0700

    update name validate, change README

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -11,6 +11,6 @@ module.exports.signupValidation = celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
     password: Joi.string().required(),
-    name: Joi.string().min(2).max(30),
+    name: Joi.string().required().min(2).max(30),
   }),
 });
