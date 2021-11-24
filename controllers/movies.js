aa4428e7a65313de7f0de00118fae1d0d74f8f2f
Michael Dokuchaev <redisooon@ya.ru>
2021-11-25 00:06:08 +0700

    fix: removeMovie from db

--- controllers/movies.js
+++ controllers/movies.js
@@ -54,7 +54,7 @@ module.exports.removeMovie = (req, res, next) => {
   const { owner } = req.user._id;
   const { movieId } = req.params;
 
-  Movie.findOne({ movieId })
+  Movie.find({ movieId, owner: req.user._id })
     .then((movie) => {
       if (movie === null) {
         throw new NotFoundError('Фильм с указанным movieId не найден');
