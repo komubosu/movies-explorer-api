1d383a409d90f6b2dde3b8d7ea5e9a1757068bda
Michael Dokuchaev <redisooon@ya.ru>
2021-11-25 17:09:50 +0700

    fix: update movie model

--- models/movie.js
+++ models/movie.js
@@ -4,26 +4,32 @@ const movieSchema = new mongoose.Schema({
   country: {
     type: String,
     required: true,
+    default: '',
   },
   director: {
     type: String,
     required: true,
+    default: '',
   },
   duration: {
     type: Number,
     required: true,
+    default: '',
   },
   year: {
     type: String,
     required: true,
+    default: '',
   },
   description: {
     type: String,
     required: true,
+    default: '',
   },
   image: {
     type: String,
     required: true,
+    default: '',
     validate: {
       validator: (v) => {
         /^https?:\/\/(www\.)?([a-z0-9]{1}[a-z0-9-]*\.?)*\.{1}[a-z0-9-]{2,8}(\/([\w#!:.?+=&%@!\-/])*)?/.test(v);
@@ -34,6 +40,7 @@ const movieSchema = new mongoose.Schema({
   trailer: {
     type: String,
     required: true,
+    default: '',
     validate: {
       validator: (v) => {
         /^https?:\/\/(www\.)?([a-z0-9]{1}[a-z0-9-]*\.?)*\.{1}[a-z0-9-]{2,8}(\/([\w#!:.?+=&%@!\-/])*)?/.test(v);
@@ -44,6 +51,7 @@ const movieSchema = new mongoose.Schema({
   thumbnail: {
     type: String,
     required: true,
+    default: '',
     validate: {
       validator: (v) => {
         /^https?:\/\/(www\.)?([a-z0-9]{1}[a-z0-9-]*\.?)*\.{1}[a-z0-9-]{2,8}(\/([\w#!:.?+=&%@!\-/])*)?/.test(v);
@@ -55,18 +63,22 @@ const movieSchema = new mongoose.Schema({
     type: mongoose.Schema.Types.ObjectId,
     ref: 'user',
     required: true,
+    default: '',
   },
   movieId: {
     type: String,
     required: true,
+    default: '',
   },
   nameRU: {
     type: String,
     required: true,
+    default: '',
   },
   nameEN: {
     type: String,
     required: true,
+    default: '',
   },
 });
 
